7b1e774ea06260623dbb995c5b50d39d44a821df
tos340 <thom@developer>
2017-01-27 15:37:48 +0100

    2017 01 27 3:36 pm
    Module vier zo goed als af, inlezen vereenvoudigd naar één functie en
    programma wat opgeschoont. Invoer komt tevens overeen met skeleton

--- Introduction to Programming/Funwithciphers/skeleton(1).py
+++ Introduction to Programming/Funwithciphers/skeleton(1).py
@@ -1,36 +1,18 @@
 '''
-Created on 26 jan. 2017
-Graded assigment module 4, from skeleton
+Created on Jan 26th, 2017
+Graded Assignment module 4, from skeleton
 @author: Thom
 '''
-
+###importing###
 import sys
 import os
-from functions import to_binary, to_decimal, binary_to_ascii, ceasar_encrypt_single_letter, ceasar_decrypt_single_letter, ceasar_encrypt_sentence, ceasar_decrypt_sentence
-from graded_assigment import vignere_encrypt_sentence, vignere_decrypt_sentence
 
-##################
+###CONSTANTs###
 ASCII_NUMBER = 256
-''' 
-def ceasar_encrypt_single_letter(letter, sleutel):
-    encrypted_character = chr((ord(letter)+sleutel)%ASCII_NUMBER)
-    return encrypted_character
 
-def ceasar_decrypt_single_letter(encrypted_character, sleutel):
-    decrypted_character = chr((ord(encrypted_character)-sleutel)%ASCII_NUMBER)
-    return decrypted_character
-
-#Now use the single letter functions for entire sentences
-
-def ceasar_encrypt_sentence(string_sentence, key):
-    
-
-def ceasar_decrypt_sentence(string_encrypted_sentence, key):
-    
-#############''' 
+###functions###
 
 # Converts a list of binary strings to ASCII message
-
 def binary_to_ascii(lst):
     msg = ""
     for b in lst:
@@ -38,22 +20,32 @@ def binary_to_ascii(lst):
         msg += chr(decimal_value)
     return msg
 
+def to_decimal(bin):
+    list = map(int, str(bin))
+    sum = int(0)
+    counter = 0
+    for element in list:
+        sum += element * 2**(int(len(list))-counter-1)
+        counter += 1
+    return sum 
+
 # Caesar cipher
+def ceasar_decrypt_single_letter(encrypted_character, sleutel):
+    decrypted_character = chr((ord(encrypted_character)-sleutel)%ASCII_NUMBER)
+    return decrypted_character
+
 def caesar_decrypt(msg, k):
     lijst = list(msg)
     decrypted_sentence = ''
     for letter in lijst:
         decrypted_letter = ceasar_decrypt_single_letter(letter, k)
         decrypted_sentence += decrypted_letter
-    #print decrypted_sentence
     return decrypted_sentence
 
 # Vigenere cipher
 def vigenere_decrypt(msg, k):
     list_encrypted_sentence = list(msg)
     list_keyword = list(k)
-    #print list_encrypted_sentence
-    #print list_keyword
     if len(list_encrypted_sentence) < len(list_keyword):
         while len(list_encrypted_sentence) < len(list_keyword):
             list_keyword = list_keyword[0:-1]
@@ -62,70 +54,49 @@ def vigenere_decrypt(msg, k):
         while len(list_encrypted_sentence) > len(list_keyword):
             list_keyword.append(list_keyword[index])
             index += 1
-    #print list_encrypted_sentence
-    #print list_keyword
-    vignere_decrypted_sentence = ''
-    teller = int(0)
+    vigenere_decrypted_sentence = ''
+    counter = int(0)
     for letter in list_encrypted_sentence:
-        vignere_decrypted_letter = ceasar_decrypt_single_letter(letter, ord(list_keyword[teller]))
-        teller +=1
-        vignere_decrypted_sentence += vignere_decrypted_letter
-    #print vignere_decrypted_sentence
-    return vignere_decrypted_sentence
+        vigenere_decrypted_letter = ceasar_decrypt_single_letter(letter, ord(list_keyword[counter]))
+        counter +=1
+        vigenere_decrypted_sentence += vigenere_decrypted_letter
+    return vigenere_decrypted_sentence
 
-def start(filename):
-    '''
-    TASK: Decrypt a list of binary-encoded encrypted messages.
-    '''
-    # TODO: read input file
-file = open('01_all.txt')
-invoer = file.read()
-list_of_sentences_to_decrypt = invoer.split('\n')
-#print list_of_sentences_to_decrypt
 
-def split_single_sentence(sentence_to_decrypt):
+#reads input and calls decrypt_caesar_or_vignere(...) function
+def split_single_sentence_and_decrypt(sentence_to_decrypt):
     encryptionmethod_keyword_binary = sentence_to_decrypt.split(':')
-    return encryptionmethod_keyword_binary
-
-def determine_encryption_method(sentence_to_decrypt):
-    encryptionmethod = split_single_sentence(sentence_to_decrypt)[0]
-    #print encryptionmethod
-    return encryptionmethod
-
-def read_keyword(sentence_to_decrypt):
-    #lijst = sentence_to_decrypt.split(':')
-    keyword = split_single_sentence(sentence_to_decrypt)[1]
-    #print keyword
-    return keyword
-
-#read_keyword(list_of_sentences_to_decrypt[0])
-
-def read_binary_code(sentence_to_decrypt):
-    binary_code = split_single_sentence(sentence_to_decrypt)[2]
-    lijst_met_binary_code = binary_code.split()
-    return lijst_met_binary_code
+    method = encryptionmethod_keyword_binary[0]
+    keyword = encryptionmethod_keyword_binary[1]
+    binary = encryptionmethod_keyword_binary[2]
+    list_with_binary_code = binary.split()
+    decrypt_caesar_or_vigenere(method, keyword, list_with_binary_code)
     
-#read_binary_code(list_of_sentences_to_decrypt[0])
-#determine_encryption_method(list_of_sentences_to_decrypt[0])
-    # TODO: Print solution
-    
-for sentence_to_decrypt in list_of_sentences_to_decrypt:
-    if determine_encryption_method(sentence_to_decrypt) == 'vigenere':
-        keyword = read_keyword(sentence_to_decrypt)
-        input_in_ascii = binary_to_ascii(read_binary_code(sentence_to_decrypt))
+
+#decrypts message using caesar or vigenere, depending on input
+def decrypt_caesar_or_vigenere(method, keyword, list_with_binary_code):
+    if method == 'vigenere':
+        input_in_ascii = binary_to_ascii(list_with_binary_code)
         decrypted_sentence = vigenere_decrypt(input_in_ascii, keyword)
         print decrypted_sentence
-    elif determine_encryption_method(sentence_to_decrypt) == 'caesar':
-        keyword = int(read_keyword(sentence_to_decrypt))
-        input_in_ascii = binary_to_ascii(read_binary_code(sentence_to_decrypt))
+    elif method == 'caesar':
+        keyword = int(keyword)
+        input_in_ascii = binary_to_ascii(list_with_binary_code)
         decrypted_sentence = caesar_decrypt(input_in_ascii, keyword)
         print decrypted_sentence
     else:
         print 'There was an error in recognizing the encryption method by decrypting \
-the sentence with input:\"%s \".'%sentence_to_decrypt
-
+the sentence with input:\"%s:%s:%s \".'%(method,keyword,list_with_binary_code)
   
+#reads file and starts the whole process
+def start(filename):
+    file = open(filename)
+    invoer = file.read()
+    list_of_sentences_to_decrypt = invoer.split('\n') 
+    for sentence_to_decrypt in list_of_sentences_to_decrypt:
+        split_single_sentence_and_decrypt(sentence_to_decrypt)
 
+###EXECUTE###
 # No need to modify this
 if __name__ == '__main__':
     # Get the first command line argument
@@ -137,3 +108,5 @@ if __name__ == '__main__':
         print("Could not find input file %s." % (filename,))
         sys.exit(1)
     start(filename)
+    
+

337391ae982f7088a71341c2698f0f5502e6086e
tos340 <thom@developer>
2017-01-31 11:56:25 +0100

    2017 01 31 11:55 am
    Newton werkt nu! (input en output functies klopt nu, return toegevoegd
    aan newton command, min ipv keer bij berekening newton)

--- Introduction to Programming/Chapter5/Newton.py
+++ Introduction to Programming/Chapter5/Newton.py
@@ -4,77 +4,73 @@ Created on 27 jan. 2017
 @author: tos340
 '''
 
-file = open('inputnewton1.txt')
+file = open('inputnewton2.txt')
 input = file.read()
 input_split = input.split('\n')
-algebraic_equation = input_split[0]
+algebraic_equation_from_file = input_split[0].split()
 number_of_repetitions = int(input_split[1])
 initial_guess_for_a_root = float(input_split[2])
 
-print algebraic_equation
+print algebraic_equation_from_file
 
 def read_function(algebraic_equation):
-    algebraic_equation_list = algebraic_equation.split()
+    #algebraic_equation_list = algebraic_equation.split()
+    algebraic_equation_list = map(float, algebraic_equation)
     coefficients = algebraic_equation_list[0:-1:2]
     exponents = algebraic_equation_list[1::2]
-    print algebraic_equation_list
-    print coefficients
-    print exponents
-    return coefficients, exponents
-
-read_function(algebraic_equation)
+    #print algebraic_equation_list
+    #print coefficients
+    #print exponents
+    return coefficients, exponents, algebraic_equation_list
 
 def derivative(algebraic_equation):
-    coefficients, exponents = read_function(algebraic_equation)
-    coefficient_derivative_list = []
-    for coefficient in coefficients:
-        coefficient_float = float(coefficient)
-        coefficient_derivative = coefficient_float * float(exponents[coefficients.index(coefficient)])
-        coefficient_derivative_list.append(coefficient_derivative)
-    exponent_derivative_list = []
-    for exponent in exponents:
-        exponent_int = int(exponent)
+    coefficients, exponents, algebraic_equation_list = read_function(algebraic_equation)
+    #coefficient_derivative_list = []
+    algebraic_equation_derivative_list = []
+    list_number_of_coefficients = range(0, len(coefficients))
+    for number in list_number_of_coefficients:
+        coefficient_float = float(coefficients[number])
+        coefficient_derivative = coefficient_float * float(exponents[number])
+        #coefficient_derivative_list.append(coefficient_derivative)
+        algebraic_equation_derivative_list.append(coefficient_derivative)
+        exponent_int = int(exponents[number])
         if exponent_int >= 1:
             exponent_derivative = exponent_int - 1
         elif exponent_int == 0:
             exponent_derivative = 0
         else: 
             print 'please give positive exponents only'
-        exponent_derivative_list.append(exponent_derivative)
-    print coefficient_derivative_list
-    print exponent_derivative_list
-        
-derivative(algebraic_equation)
+        algebraic_equation_derivative_list.append(exponent_derivative)
+    return algebraic_equation_derivative_list
 
-def function_value_at_x(algebraic_equiation, x):
-    coefficients, exponents = (read_function(algebraic_equation))
+
+def function_value_at_x(algebraic_equation, x):
+    coefficients, exponents, algebraic_equation_list = (read_function(algebraic_equation))
     sum = 0
-    for exponent in exponents:
-        exponent_float = float(exponent)
-        exponent_of_x = x ** (exponent_float)
-        sum += exponent_of_x * float(coefficients[exponents.index(exponent)])
-    return float(sum)
-
-
-sum = function_value_at_x(algebraic_equation, initial_guess_for_a_root)
-print 'test function function_value_at_x: %f'%sum
-print sum
+    list_number_of_coefficients = range(0, len(coefficients))
+    for number in list_number_of_coefficients:
+        exponent_float = exponents[number] #float command can be removed?!?
+        exponent_of_x = (x) ** (exponent_float)
+        sum += (exponent_of_x * (coefficients[number]))
+    return sum
 
 def newton(algebraic_equation, number_of_repetitions, x_n):
+    print 'dit is x_n: %f' %x_n
     if number_of_repetitions == 0:
         return x_n
-    x_n_plus_1 = x_n * ((function_value_at_x(algebraic_equation, x_n))/  (function_value_at_x(derivative(algebraic_equation), x_n)))
-    number_of_repetitions_new = int(number_of_repetitions - 1)
-    newton(algebraic_equation, number_of_repetitions_new, x_n_plus_1)
+    x_n_plus_1 = x_n - ((function_value_at_x(algebraic_equation, x_n))/  (function_value_at_x(derivative(algebraic_equation), x_n)))
+    number_of_repetitions_new = number_of_repetitions - 1
+    return newton(algebraic_equation, number_of_repetitions_new, x_n_plus_1)
 
-print 'newton test:'    
-uitkomst = newton(algebraic_equation, number_of_repetitions, initial_guess_for_a_root)
+print 'Newton test:' 
+
+coefficient_read, exponent_read, algebraic_equation_read = read_function(algebraic_equation_from_file)   
+uitkomst = newton(algebraic_equation_read, number_of_repetitions, initial_guess_for_a_root)
 print 'This is your initial guess for the root: %f, this is your algebraic equation: %s, \
-this is your number of repetitions: %s, this is your answer: .' %(initial_guess_for_a_root, \
-algebraic_equation, number_of_repetitions)
-print 'This is the answer: %s'%uitkomst
+this is your number of repetitions: %s, this is your answer: %s.' %(initial_guess_for_a_root, \
+algebraic_equation_from_file, number_of_repetitions, uitkomst)
+
     
-                        
 
 
 #for repetition in range(int(number_of_repetitions)):

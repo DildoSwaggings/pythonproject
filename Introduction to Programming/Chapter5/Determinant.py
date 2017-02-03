305be49584a593acd0316fec0c79fc3947131a36
tos340 <thom@developer>
2017-02-03 16:25:41 +0100

    2017 02 04 4:24 pm
    determinant opgeschoond: aparte functie voor 2x2 matrix verwijderd en
    start functie gemaakt. newton opgeschoont, onnodige variabele
    algebraic_equation_list verwijderd en startfunctie gemaakt

--- Introduction to Programming/Chapter5/Determinant.py
+++ Introduction to Programming/Chapter5/Determinant.py
@@ -3,9 +3,6 @@ Created on 31 jan. 2017
 Assigment Determinant
 @author: Thom, tos340
 '''
-####READING FILE AND MATRICES IN FILE###
-file = open('matrix3.txt').read()
-list_of_matrices_from_file = file.split('=\n')
 
 ###functions###
 def read_matrix(list_of_matricses, number):
@@ -22,20 +19,6 @@ def print_matrix(matrix):
         for element in row:
             print element,
         print '' #This makes a new line for a new row
-        
-def determinant_two_by_two_matrix(two_by_two_matrix):
-    a, b, c, d = read_elements_two_by_two_matrix(two_by_two_matrix)
-    determinant = (a * d) - (b * c)
-    return determinant
-
-def read_elements_two_by_two_matrix(two_by_two_matrix):
-    row_one = two_by_two_matrix[0]
-    row_two = two_by_two_matrix[1]
-    a = row_one[0]
-    b = row_one[1]
-    c = row_two[0]
-    d = row_two[1]
-    return a, b, c, d
 
 def check_and_give_lenght_n_by_n_matrix(n_by_n_matrix):
     number_of_rows = len(n_by_n_matrix)
@@ -53,41 +36,24 @@ def determinant_n_by_n_matrix(n_by_n_matrix):
     lenght_matrix = check_and_give_lenght_n_by_n_matrix(n_by_n_matrix)
     if lenght_matrix == 1:
         return int(n_by_n_matrix[0][0])
-    if lenght_matrix == 2:
-        return determinant_two_by_two_matrix(n_by_n_matrix)
     row_one = n_by_n_matrix[0]
     matrix_without_row_one = n_by_n_matrix[1: lenght_matrix]
     determinant = 0
     for number in range(len(row_one)):
-        determinant += (-1)**(number+1) * row_one[number] * determinant_n_by_n_matrix(give_matrix_with_deleted_column(number, matrix_without_row_one))
+        determinant += (-1)**(number) * row_one[number] * determinant_n_by_n_matrix(give_matrix_with_deleted_column(number, matrix_without_row_one))
     return determinant
 
-def start(list_of_matrices, number_of_matrix_in_file):
-    matrix_from_file =read_matrix(list_of_matrices, number_of_matrix_in_file)
+def read_file_and_start(filename, number_of_matrix_in_file):
+    file = open(filename).read()
+    list_of_matrices_from_file = file.split('=\n')
+    matrix_from_file = read_matrix(list_of_matrices_from_file, number_of_matrix_in_file)
     determinant = determinant_n_by_n_matrix(matrix_from_file)
     print 'The determinant of the matrix '
     print_matrix(matrix_from_file)
     print 'is \n%.2f'%(determinant)
     
-    
-    
+     
 ###execute###
 if __name__ == '__main__':
-    start(list_of_matrices_from_file, 0)
-    
-'''   
-#VRAGEN OP WERKCOLLEGE
-#inlezen zdd spaties aan einde bij input bestand wegvallen en enters READLINES?!
-#staat return in determinant_n_by_n_matrix() op goede plek??
-#for delete_column: begint nu bij column nummer 0, beter om bij 1 te beginnen??
-#TESTING:
-result = read_matrix(list_of_matrices_from_file, 1)
-print result
-print_matrix(result)
-determinant = determinant_two_by_two_matrix(result)
-print determinant
-print read_elements_two_by_two_matrix(result)
-print check_and_give_lenght_matrix(result)
-print delete_column(2, result)
-print determinant_n_by_n_matrix(result)
-#'''
+    read_file_and_start('matrix3.txt', 1)
+
--- Introduction to Programming/Chapter5/Newton.py
+++ Introduction to Programming/Chapter5/Newton.py
@@ -1,37 +1,23 @@
 '''
 Created on 27 jan. 2017
-
-@author: tos340
+Module 5, Assignment Newton
+@author: Thom, tos340
 '''
 
-file = open('inputnewton2.txt')
-input = file.read()
-input_split = input.split('\n')
-algebraic_equation_from_file = input_split[0].split()
-number_of_repetitions = int(input_split[1])
-initial_guess_for_a_root = float(input_split[2])
-
-print algebraic_equation_from_file
-
+###functions###
 def read_function(algebraic_equation):
-    #algebraic_equation_list = algebraic_equation.split()
     algebraic_equation_list = map(float, algebraic_equation)
     coefficients = algebraic_equation_list[0:-1:2]
     exponents = algebraic_equation_list[1::2]
-    #print algebraic_equation_list
-    #print coefficients
-    #print exponents
-    return coefficients, exponents, algebraic_equation_list
+    return coefficients, exponents
 
 def derivative(algebraic_equation):
-    coefficients, exponents, algebraic_equation_list = read_function(algebraic_equation)
-    #coefficient_derivative_list = []
+    coefficients, exponents = read_function(algebraic_equation)
     algebraic_equation_derivative_list = []
     list_number_of_coefficients = range(0, len(coefficients))
     for number in list_number_of_coefficients:
         coefficient_float = float(coefficients[number])
         coefficient_derivative = coefficient_float * float(exponents[number])
-        #coefficient_derivative_list.append(coefficient_derivative)
         algebraic_equation_derivative_list.append(coefficient_derivative)
         exponent_int = int(exponents[number])
         if exponent_int >= 1:
@@ -45,35 +31,42 @@ def derivative(algebraic_equation):
 
 
 def function_value_at_x(algebraic_equation, x):
-    coefficients, exponents, algebraic_equation_list = (read_function(algebraic_equation))
+    coefficients, exponents = (read_function(algebraic_equation))
     sum = 0
     list_number_of_coefficients = range(0, len(coefficients))
     for number in list_number_of_coefficients:
-        exponent_float = exponents[number] #float command can be removed?!?
+        exponent_float = float(exponents[number])
         exponent_of_x = (x) ** (exponent_float)
         sum += (exponent_of_x * (coefficients[number]))
     return sum
 
 def newton(algebraic_equation, number_of_repetitions, x_n):
-    print 'dit is x_n: %f' %x_n
     if number_of_repetitions == 0:
         return x_n
     x_n_plus_1 = x_n - ((function_value_at_x(algebraic_equation, x_n))/  (function_value_at_x(derivative(algebraic_equation), x_n)))
     number_of_repetitions_new = number_of_repetitions - 1
     return newton(algebraic_equation, number_of_repetitions_new, x_n_plus_1)
 
-print 'Newton test:' 
-
-coefficient_read, exponent_read, algebraic_equation_read = read_function(algebraic_equation_from_file)   
-uitkomst = newton(algebraic_equation_read, number_of_repetitions, initial_guess_for_a_root)
-print 'This is your initial guess for the root: %f, this is your algebraic equation: %s, \
-this is your number of repetitions: %s, this is your answer: %s.' %(initial_guess_for_a_root, \
-algebraic_equation_from_file, number_of_repetitions, uitkomst)
-
+def read_file_and_start(filename):
+    file = open(filename)
+    input = file.read()
+    input_split = input.split('\n')
+    algebraic_equation_from_file = input_split[0].split()
+    number_of_repetitions = int(input_split[1])
+    initial_guess_for_a_root = float(input_split[2])
+    #
+    uitkomst = newton(algebraic_equation_from_file, number_of_repetitions, initial_guess_for_a_root)
+    print 'This is your initial guess for the root: %.2f, this is your algebraic equation: %s,\
+    this is your number of repetitions: %s, this is your answer: %.1f.' %(initial_guess_for_a_root, \
+    algebraic_equation_from_file, number_of_repetitions, uitkomst)
+    
+    
+###execute###
+if __name__ == '__main__':
+    read_file_and_start('inputnewton2.txt')
+    
+    
     
 
-
-#for repetition in range(int(number_of_repetitions)):
- #   x = 
     
 
